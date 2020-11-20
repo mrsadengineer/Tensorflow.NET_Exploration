3236e0a67e491897853b55ef55d1dfbd85b00041
Sam <dsaeresearch@gmail.com>
2020-11-19 20:13:17 -0500

    Included helloworld

--- ConsoleApp_TensorflowSharpHelloWorldNetFk/Program.cs
+++ ConsoleApp_TensorflowSharpHelloWorldNetFk/Program.cs
@@ -4,12 +4,72 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+using static Tensorflow.Binding;
+
 namespace ConsoleApp_TensorflowSharpHelloWorldNetFk
 {
     class Program
     {
+        public int Priority { get; set; } = 100;
+
+        /// <summary>
+        /// Example name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// True to run example
+        /// </summary>
+        public bool Enabled { get; set; } = true;
+
+        /// <summary>
+        /// Set true to import the computation graph instead of building it.
+        /// </summary>
+        public bool IsImportingGraph { get; set; } = false;
+
+
+      //  public ExampleConfig Config { get; set; }
+
+
         static void Main(string[] args)
         {
+            var hello = tf.constant("Hello, TensorFlow!");
+            Console.WriteLine(hello);
+            Console.WriteLine("test here");
+            Console.ReadKey();
+
+
+            //bool didRun = Run();
+            // Console.WriteLine("write here");
+            // Console.ReadKey();
+
+        }
+
+
+      //  public ExampleConfig InitConfig()
+      //  => Config = new ExampleConfig
+      //  {
+     //       Name = "Hello World",
+     //       Priority = 1
+       // };
+
+        public static bool Run()
+        {
+            /* Create a Constant op
+               The op is added as a node to the default graph.
+            
+               The value returned by the constructor represents the output
+               of the Constant op. */
+            var str = "Hello, TensorFlow.NET!";
+            var hello = tf.constant(str);
+
+            // tf.Tensor: shape=(), dtype=string, numpy=b'Hello, TensorFlow.NET!'
+            print(hello);
+
+            var tensor = (string)hello.numpy();
+
+            return str == tensor;
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,36 @@
+# TensorFlow.NET Exploration
+
+Introduction - the project's aim
+Testing out Tensorflow.NET.  
+
+https://github.com/SciSharp/TensorFlow.NET 
+
+## Technologies
+Project is created with:
+*TensorFlow.NET
+
+Placement for Future README Sub sections
+Launch/ To Setup
+Table of contents
+Illustrations
+Scope of functionalities
+Examples of use
+Project status
+Sources
+Other information
+
+## Documentation
+
+Tensorflow.NET requires the build to be x64.
+
+To install TF.NET and TensorFlow Binrary with NuGet:
+
+### install tensorflow C# binding
+PM> Install-Package TensorFlow.NET
+
+### Install tensorflow binary
+### For CPU version
+PM> Install-Package SciSharp.TensorFlow.Redist
+
+### For GPU version (CUDA and cuDNN are required)
+PM> Install-Package SciSharp.TensorFlow.Redist-Windows-GPU
